a87addf719e81f4c92d30aa3181184f5a59e8a7e
brarkhushpreet <brarkhushpreet@users.noreply.github.com>
2024-02-24 13:57:16 +0530

    new changes

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,15 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div className="flex ">
+   
+    <UserButton
+    afterSignOutUrl="/"
+    />
+     <ModeToggle/>
+    </div>
+  );
+}
